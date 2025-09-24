3c8110f015b1368d310eb90693edf35a96daea71
Shoukat-Ali007 <shoukatbhatt54@gmail.com>
2025-09-24 17:41:26 +0500

    persistent per-thread pipelines; feed multiple lines into one pipeline per worker and stream outputs to receiver

--- parapipe.c
+++ parapipe.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <errno.h>
+
 
 struct worker_arg {
   char **commands;
@@ -17,114 +19,303 @@ struct worker_arg {
  
 };
 
-void *worker(void *arg) {
-    struct worker_arg *warg = (struct worker_arg *)arg;
 
- // For each input line
-  for(int l = warg->start_idx; l < warg->end_idx; l++) {
-     char *input = warg->lines[l];
-     int prev_fd[2];
-     int next_fd[2];
-     int last_output = -1; 
+//queue for streaming results to receiver
+typedef struct Node {
+    char *data;
+    struct Node *next;
+} Node;
 
-     pid_t pids[64]; 
-     int pid_count = 0;
+static Node *q_head = NULL;
+static Node *q_tail = NULL;
+static pthread_mutex_t q_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t q_cond = PTHREAD_COND_INITIALIZER;
+static int workers_running = 0; // number of worker threads still running
 
+void enqueue_result(char *s) {
+    Node *n = malloc(sizeof(Node));
+    n->data = s; // take ownership of string
+    n->next = NULL;
 
-   for (int c = 0; c < warg->cmd_count; c++) {
-     if (c != warg->cmd_count - 1) pipe(next_fd); 
-
-     pid_t pid = fork();
-      if (pid == 0) {
-  //this is Child  
-         if (c > 0) {
-       dup2(prev_fd[0], STDIN_FILENO); 
-      close(prev_fd[0]);
-       close(prev_fd[1]);
-                } else {
- // first read from input line
-      int input_pipe[2];
-     pipe(input_pipe);
-     write(input_pipe[1], input, strlen(input));
-      close(input_pipe[1]);
-    dup2(input_pipe[0], STDIN_FILENO);
-       close(input_pipe[0]);
-        }
-
-    if (c != warg->cmd_count - 1) {
-     dup2(next_fd[1], STDOUT_FILENO); 
-     close(next_fd[0]);
-    close(next_fd[1]);
-                }
-
-  // Execute command
-    char *cmd_args[10];
-    char *token = strtok(warg->commands[c], " ");
-   int idx = 0;
-     while (token) {
-       cmd_args[idx++] = token;
-       token = strtok(NULL, " ");
-        }
-   cmd_args[idx] = NULL;
-
-    execvp(cmd_args[0], cmd_args);
-    perror("execvp failed");
-    exit(1);
-  } else {
-   
-//Parent 
-
- pids[pid_count++] = pid; 
-
-    if (c > 0) {
-      close(prev_fd[0]);
-      close(prev_fd[1]);
-     }
-      if (c != warg->cmd_count - 1) {
-    prev_fd[0] = next_fd[0];
-    prev_fd[1] = next_fd[1];
-     } else {
-      last_output = prev_fd[0]; 
-       }
-    
+    pthread_mutex_lock(&q_mutex);
+    if (!q_head) {
+        q_head = q_tail = n;
+    } else {
+        q_tail->next = n;
+        q_tail = n;
     }
-   }
-for (int i = 0; i < pid_count; i++) {
-            waitpid(pids[i], NULL, 0);
+    pthread_cond_signal(&q_cond);
+    pthread_mutex_unlock(&q_mutex);
+}
+
+char *dequeue_result() {
+    pthread_mutex_lock(&q_mutex);
+    Node *n = q_head;
+    if (!n) {
+        pthread_mutex_unlock(&q_mutex);
+        return NULL;
+    }
+    q_head = n->next;
+    if (!q_head) q_tail = NULL;
+    pthread_mutex_unlock(&q_mutex);
+
+    char *s = n->data;
+    free(n);
+    return s;
+}
+
+void *worker(void *arg) {
+struct worker_arg *warg = (struct worker_arg *)arg;
+  struct worker_arg *wa = (struct worker_arg *)arg;
+
+ 
+   int num_cmds = wa->cmd_count;
+    if (num_cmds <= 0) {
+       goto done;
+      }
+
+ //create pipes between commands
+    int (*pipes)[2] = NULL;
+    if (num_cmds > 1) {
+        pipes = malloc(sizeof(int[2]) * (num_cmds - 1));
+        for (int i = 0; i < num_cmds - 1; i++) {
+         if (pipe(pipes[i]) < 0) {
+             perror("pipe");
+              
+              for (int j = 0; j < i; j++) {
+ close(pipes[j][0]);
+ close(pipes[j][1]);
+ }
+          free(pipes);
+           goto done;
+         }
+       }
+    }
+
+ //create in_pipe for first command 
+    int in_pipe[2];
+    if (pipe(in_pipe) < 0) {
+        perror("pipe");
+        if (pipes) {
+            for (int i = 0; i < num_cmds - 1; i++) {
+ close(pipes[i][0]);
+ close(pipes[i][1]);
+ }
+            free(pipes);
+        }
+        goto done;
+    }
+
+ //create out_pipe 
+    int out_pipe[2];
+    if (pipe(out_pipe) < 0) {
+        perror("pipe");
+    close(in_pipe[0]);
+    close(in_pipe[1]);
+        if (pipes) {
+       for (int i = 0; i < num_cmds - 1; i++) {
+ close(pipes[i][0]);
+ close(pipes[i][1]);
+     }
+          free(pipes);
+       }
+      goto done;
+    }
+
+
+//fork all commands 
+    pid_t *pids = calloc(num_cmds, sizeof(pid_t));
+    for (int c = 0; c < num_cmds; c++) {
+      pid_t pid = fork();
+      if (pid < 0) {
+        perror("fork");
+      
+       pids[c] = -1;
+        continue;
+      }
+      if (pid == 0) {
+            
+        if (c == 0) {
+          
+        dup2(in_pipe[0], STDIN_FILENO);
+            
+         } else {
+          
+           dup2(pipes[c-1][0], STDIN_FILENO);
+           
         }
 
-
-  //non blocking Read output from last command 
-    if (last_output != -1) {
-     fcntl(last_output, F_SETFL, O_NONBLOCK);
-          char buf[1024];
-         int total = 0;
-         warg->results[l] = calloc(1, 1);
-
-           while (1) {
-              int n = read(last_output, buf, sizeof(buf) - 1);
-               if (n > 0) {
-                  buf[n] = '\0';
-                  total += n;
-                   warg->results[l] = realloc(warg->results[l], total + 1);
-                    strcat(warg->results[l], buf);
-               } else {
-                    break;
-                }
+   
+  if (c == num_cmds - 1) {
+          
+       dup2(out_pipe[1], STDOUT_FILENO);
+         
+       } else{
+         
+       dup2(pipes[c][1], STDOUT_FILENO);
+              
             }
 
-            close(last_output);
+//close all unused fds in child
+close(in_pipe[0]);
+ close(in_pipe[1]);
+  close(out_pipe[0]); 
+ close(out_pipe[1]);
+      if (pipes) {
+         for (int j = 0; j < num_cmds - 1; j++) {
+            close(pipes[j][0]); close(pipes[j][1]);
+                }
+         } 
+
+
+ //prepare argv 
+    char *cmddup = strdup(wa->commands[c]);
+    char *argv[64];
+     int ai = 0;
+     char *tok = strtok(cmddup, " ");
+         while (tok && ai < 63) {
+             argv[ai++] = tok;
+              tok = strtok(NULL, " ");
+                }
+         argv[ai] = NULL;
+
+ //exec
+    execvp(argv[0], argv);
+     
+      perror("execvp failed");
+         free(cmddup);
+         _exit(1);
+          } else {
+//parent
+      pids[c] = pid;
         }
     }
 
+   
+//closed unused fds
+close(in_pipe[0]);
+    
+    if (pipes) {
+        for (int j = 0; j < num_cmds - 1; j++) {
+           close(pipes[j][0]);
+            close(pipes[j][1]);
+        }
+    }
+close(out_pipe[1]); 
+
+for (int l = wa->start_idx; l < wa->end_idx; l++) {
+ char *line = wa->lines[l];
+  if (!line) continue;
+   ssize_t towrite = strlen(line);
+   ssize_t written = 0;
+      while (written < towrite) {
+         ssize_t w = write(in_pipe[1], line + written, towrite - written);
+           if (w < 0) {
+              if (errno == EINTR) continue;
+                perror("write to pipeline");
+                break;
+            }
+            written += w;
+        }
+    }
+    
+    close(in_pipe[1]); 
+
+//read 
+    char readbuf[2048];
+    ssize_t nread;
+    char *acc = NULL;
+    size_t acc_len = 0;
+
+    while ((nread = read(out_pipe[0], readbuf, sizeof(readbuf))) > 0) {
+
+     char *newacc = realloc(acc, acc_len + nread + 1);    
+    if (!newacc) {
+     perror("realloc");
+    free(acc);
+     acc = NULL; acc_len = 0;
+        break;
+            }
+
+        acc = newacc;
+        memcpy(acc + acc_len, readbuf, nread);
+        acc_len += nread;
+        acc[acc_len] = '\0';
+
+        char *start = acc;
+        char *newline;
+        while ((newline = memchr(start, '\n', acc + acc_len - start))) {
+            size_t linelen = newline - start + 1;
+            char *outline = malloc(linelen + 1);
+            memcpy(outline, start, linelen);
+            outline[linelen] = '\0';
+            enqueue_result(outline);
+            start = newline + 1;
+        }
+
+        size_t remain = acc + acc_len - start;
+        if (remain > 0) memmove(acc, start, remain);
+        acc_len = remain;
+        if (acc) acc[acc_len] = '\0';
+    }
+
+    if (acc && acc_len > 0) {
+        char *final = malloc(acc_len + 2);
+        memcpy(final, acc, acc_len);
+        final[acc_len] = '\n';
+        final[acc_len + 1] = '\0';
+        enqueue_result(final);
+    }
+    free(acc);
+    close(out_pipe[0]);
+
+    // Wait for children
+    for (int c = 0; c < num_cmds; c++) {
+      if (pids[c] > 0) waitpid(pids[c], NULL, 0);
+      }
+    free(pids);
+    if (pipes) free(pipes);
+
+ done:
+   pthread_mutex_lock(&q_mutex);
+    workers_running--;
+    pthread_cond_signal(&q_cond);
+    pthread_mutex_unlock(&q_mutex);
+
+
     return NULL;
-}
+                 }
+
 
 //receiver
 void *receiver(void *arg) {
-char **results = (char **)arg;
-    for (int i = 0; results[i]; i++) {
-        printf("%s", results[i]); 
+ (void)arg; 
+    while (1) {
+      pthread_mutex_lock(&q_mutex);
+      while (!q_head && workers_running > 0) {
+          pthread_cond_wait(&q_cond, &q_mutex);
+          }
+
+ //if queue empty 
+    if (!q_head && workers_running == 0) {
+         pthread_mutex_unlock(&q_mutex);
+             break;
+            }
+//else dequeue one
+    Node *n = q_head;
+       if (n) {
+          q_head = n->next;
+           if (!q_head) q_tail = NULL;
+          }
+   pthread_mutex_unlock(&q_mutex);
+
+     if (n) {
+ //print and free node
+     printf("%s", n->data);
+     fflush(stdout); fflush(stdout); 
+      free(n->data);
+      free(n);
+        }
     }    
 
     return NULL;
@@ -200,31 +391,51 @@ char **all_lines = NULL;
  char **results = calloc(count + 1, sizeof(char*));
 
 //create worker thread
-pthread_t workers[n];
+ pthread_t *workers = malloc(sizeof(pthread_t) * n);
     int lines_per_thread = (count + n - 1) / n;
-for (int i = 0; i < n; i++) {
+
+   pthread_mutex_lock(&q_mutex);
+   workers_running = 0;
+   pthread_mutex_unlock(&q_mutex);
+
+ int worker_idx = 0;
+ for (int i = 0; i < n; i++) {
+        int start_idx = i * lines_per_thread;
+        int end_idx = (i+1) * lines_per_thread;
+        if (start_idx >= count) break; 
+
+        if (end_idx > count) end_idx = count;
+
         struct worker_arg *warg = malloc(sizeof(struct worker_arg));
         warg->commands = commands;
         warg->cmd_count = cmd_count;
         warg->lines = all_lines;
-        warg->start_idx = i * lines_per_thread;
-        warg->end_idx = (i + 1) * lines_per_thread;
-        if (warg->end_idx > count) warg->end_idx = count;
+        warg->start_idx = start_idx;
+        warg->end_idx = end_idx;
         warg->results = results;
 
-        pthread_create(&workers[i], NULL, worker, warg);
+        pthread_mutex_lock(&q_mutex);
+        workers_running++; 
+        pthread_mutex_unlock(&q_mutex);
+
+        pthread_create(&workers[worker_idx++], NULL, worker, warg);
     }
-
-
-    for (int i = 0; i < n; i++) {
-       pthread_join(workers[i], NULL);
-    } 
-
 
 
 //one reveiver thread
 pthread_t receiv;
     pthread_create(&receiv, NULL, receiver, results);
+
+    // join workers
+    for (int i = 0; i < n; i++) {
+        pthread_join(workers[i], NULL);
+    }
+
+    pthread_mutex_lock(&q_mutex);
+    pthread_cond_signal(&q_cond);
+    pthread_mutex_unlock(&q_mutex);
+
+//join receiver
     pthread_join(receiv, NULL);
 
 
@@ -235,8 +446,9 @@ pthread_t receiv;
     free(commands);
     for (int i = 0; i < count; i++) free(results[i]);
     free(results);
+     free(workers);
 
-  printf("Threads: %d\n",n);
+   printf("Threads: %d\n", worker_idx);
   
 
 
